cbf320c181ba3af2e55b432145ce027f680a2c4b
Samitti <samuelteweldebrhan@gmail.com>
2022-01-19 15:09:09 +0300

    add pizza list section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Featured from '../component/Featured';
+import PizzaList from '../component/PizzaList';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      <PizzaList />
     </div>
   )
 }

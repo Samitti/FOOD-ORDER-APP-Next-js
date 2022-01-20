f03565f3ee9a8415e7f7d41c5838c9b54ce0f467
Samitti <samuelteweldebrhan@gmail.com>
2022-01-20 12:28:14 +0300

    add footer section

---
+++ pages/product/[id].jsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+import styles from "../../styles/product.module.css";
+
+const Product = () => {
+    const [size, setSize] = useState(0);
+    const pizza = {
+        id: 1,
+        img: "/img/pizza.png",
+        name: "CAMPAGNOLA",
+        price: [19.90, 23.90, 27.90],
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
+    }
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <div className={styles.imgContainer}>
+                    <Image src={pizza.img} objectFit='contain' layout='fill' alt='' />
+                </div>
+            </div>
+            <div className={styles.right}>
+                <h1 className={styles.title}> {pizza.name}</h1>
+                <span  className={styles.price}>${pizza.price[size]}</span>
+                <p className={styles.desc}>{ pizza.desc}</p>
+                <h3 className={styles.choose}>Choose the size</h3>
+                <div className={styles.sizes}>
+                    <div className={styles.size}>
+                        <Image src="/img/size.png" layout="fill" alt='' />
+                        <span className={styles.number}>Small</span>
+                    </div>
+                    <div className={styles.size}>
+                        <Image src="/img/size.png" layout="fill" alt='' />
+                        <span className={styles.number}>Medium</span>
+                    </div>
+                    <div className={styles.size}>
+                        <Image src="/img/size.png" layout="fill" alt='' />
+                        <span className={styles.number}>Large</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
